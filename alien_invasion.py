083854678b3396f0719dd4177f5d1d93297056f6
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-10-13 19:40:08 -0400

    Added game window and game loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,3 +4,25 @@ import pygame
 class AlienInvasion:
     """ Overall class to manage game assets and behavior """
     
+    def _init_(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.pygame.display.set_caption("Alien Invasion", icontitle=None)
+        
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.git():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+        
+if _name_ == '_main_':
+    # Make a game instance, and run game.
+    ai = AlienInvasion()
+    ai.run_game()
