3679c537106d80ec597e895ee614b4da1ad3e4fa
Lawrence C <lawrencecaudle870@gmail.com>
2021-12-22 21:30:44 -0500

    added paraenthesis

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,7 +101,7 @@ class AlienInvasion:
 
         if collisions:
             self.stats.score += self.settings.alien_points
-            self.sb.prep_score
+            self.sb.prep_score()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
