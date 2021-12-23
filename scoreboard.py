b968715087bdf7c44081a696ae773c996288cd45
Lawrence C <lawrencecaudle870@gmail.com>
2021-12-23 00:13:13 -0500

    added colons at line 61

--- scoreboard.py
+++ scoreboard.py
@@ -47,7 +47,7 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
         
-    def prep_level(self)
+    def prep_level(self):
         """Turn the level into a rendered image."""
         level_str = str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
